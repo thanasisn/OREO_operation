a940884020dd74b1cd3417dedf65a948b2203666
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-03-05 16:31:42 +0200

    add helper functions

---
+++ DOMOS/domos_functions/size_to_human.py
@@ -0,0 +1,7 @@
+
+def size_to_human(num, suffix="B"):
+    for unit in (" ", " Ki", " Mi", " Gi", " Ti", " Pi", " Ei", " Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f} {unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f} Yi{suffix}"
