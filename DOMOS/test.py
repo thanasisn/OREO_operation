61f265ee489a46c56c20f02a431d74e363fe8f76
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-04-09 09:06:01 +0300

    minor fixes and cleanup

--- DOMOS/test.py
+++ DOMOS/test.py
@@ -5,6 +5,10 @@ import numpy    as np
 import os
 import sys
 import glob
+import tqdm
+import time
+import pathlib
+import subprocess
 
 ##  Load project functions  --------------------------------------------------
 sys.path.append("../")
@@ -15,6 +19,7 @@ import oreo_mod.calc  as Oc
 
 
 
+Ou.source_code_hash("/home/athan/OREO/operation/DOMOS/Step_01_regrid_ERA5.py")
 
 
 ##  Load configuration profile by host name  ---------------------------------
@@ -22,9 +27,10 @@ cnf = Ou.get_configs(
         Ou.parse_arguments(run_profiles_folder = "../run_profiles").profile
     )
 
-print(cnf)
-
-
-
-
-
+# for outer in [10, 20, 30, 40, 50]:
+#     print(outer,"\n")
+#     for inner in tqdm.tqdm(range(outer), desc="This file", dynamic_ncols = True):
+#         print(inner,"\n")
+#         time.sleep(1)
+# print("done!")
+#
